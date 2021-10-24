63babce47b3b428396daec98d72c570881f099d2
DESKTOP-RIEF844\PC <android172unity@gmail.com>
2021-10-24 17:28:32 +0200

    Created basic structure to the front end look of the login and register functionalities.

---
+++ frontend/app/src/main/java/com/example/skucise/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.skucise
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
--- frontend/app/src/main/java/com/example/skucise/MainActivity.kt
+++ frontend/app/src/main/java/com/example/skucise/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.skucise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login_fragment.setOnClickListener {
+            changeActiveLRFragment(LoginFragment())
+        }
+        btn_register_fragment.setOnClickListener {
+            changeActiveLRFragment(RegisterFragment())
+        }
+
+        // Set default fragment
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frc_login_or_register, LoginFragment())
+        fragmentTransaction.commit()
+    }
+
+    private fun changeActiveLRFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frc_login_or_register, fragment)
+        fragmentTransaction.commit()
     }
 }
