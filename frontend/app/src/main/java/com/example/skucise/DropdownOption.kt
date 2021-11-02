768982ca66a26c786e11e65b550a345ded4735e5
Dejan Belic <android172unity@gmail.com>
2021-11-02 19:48:03 +0100

    - Added account settings dropdown menu functionality

---
+++ frontend/app/src/main/java/com/example/skucise/DropdownOption.kt
@@ -0,0 +1,8 @@
+package com.example.skucise
+
+import android.view.View
+
+data class DropdownOption(
+    val name: String,
+    val action: View.OnClickListener
+)
--- frontend/app/src/main/java/com/example/skucise/activities/NavigationActivity.kt
+++ frontend/app/src/main/java/com/example/skucise/activities/NavigationActivity.kt
@@ -1,11 +1,14 @@
 package com.example.skucise.activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.skucise.AccountDropdownAdapter
+import com.example.skucise.DropdownOption
 import com.example.skucise.R
-import com.example.skucise.fragments.FrontPageFragment
 import kotlinx.android.synthetic.main.activity_navigation.*
 
 class NavigationActivity : AppCompatActivity() {
@@ -13,6 +16,46 @@ class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+
+        btn_account_dd_toggle.setOnClickListener {
+            if (drop_down_account.visibility == View.GONE) {
+                drop_down_account.visibility = View.VISIBLE
+            }
+            else {
+                drop_down_account.visibility = View.GONE
+            }
+        }
+
+
+        val dropdownOptions = mutableListOf<DropdownOption>()
+        dropdownOptions.add(DropdownOption(
+            "Moj nalog",
+            {}
+        ))
+        dropdownOptions.add(DropdownOption(
+            "Moji oglasi",
+            {}
+        ))
+        dropdownOptions.add(DropdownOption(
+            "Označeni oglasi",
+            {}
+        ))
+        dropdownOptions.add(DropdownOption(
+            "Poruke",
+            {}
+        ))
+        dropdownOptions.add(DropdownOption(
+            "Kalendar",
+            {}
+        ))
+        dropdownOptions.add(DropdownOption(
+            "Odjavi se",
+            {}
+        ))
+
+        val accountDropdownAdapter = AccountDropdownAdapter(dropdownOptions)
+        rcv_dd_options.adapter = accountDropdownAdapter
+        rcv_dd_options.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onStart() {
